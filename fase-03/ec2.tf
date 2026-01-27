22ae929413deb8d8ecb13598b9caa9f9621affae
Carlos Gil <carlosgilmds@gmail.com>
2026-01-27 19:44:11 -0300

    add workflow

---
+++ fase-03/ec2.tf
@@ -0,0 +1,55 @@
+resource "aws_instance" "website_server" {
+  ami                    = "ami-03ea746da1a2e36e7"
+  instance_type          = "t3.micro"
+  key_name               = "chave-site-prod"
+  vpc_security_group_ids = [aws_security_group.website_sg.id]
+  iam_instance_profile   = "ECR-EC2-role"
+
+  tags = {
+    Name        = "website-server"
+    Provisioned = "Terraform"
+    Cliente     = "Gil"
+  }
+}
+
+## Security Group
+resource "aws_security_group" "website_sg" {
+  name   = "website-sg"
+  vpc_id = "vpc-011371d8c9bfb7a5b"
+  tags = {
+    Name        = "website-sg"
+    Provisioned = "Terraform"
+    Cliente     = "Gil"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "186.227.217.137/32"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_http_outbound" {
+  security_group_id = aws_security_group.website_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = -1
+}
